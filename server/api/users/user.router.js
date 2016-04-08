721c3d5d7ef9dcfcef353bbd423051120362467d
John Nieves <karn09@gmail.com>
2016-04-07 21:11:46 -0400

    removed user password being returned

--- server/api/users/user.router.js
+++ server/api/users/user.router.js
@@ -6,55 +6,60 @@ var router = require('express').Router(),
 var HttpError = require('../../utils/HttpError');
 var User = require('./user.model');
 
-router.param('id', function (req, res, next, id) {
-	User.findById(id).exec()
-	.then(function (user) {
-		if (!user) throw HttpError(404);
-		req.requestedUser = user;
-		next();
+router.param('id', function(req, res, next, id) {
+  User.findById(id).exec()
+	.then(function(user) {
+  if (!user) throw HttpError(404);
+  req.requestedUser = user;
+  next();
 	})
 	.then(null, next);
 });
 
-router.get('/', function (req, res, next) {
-	User.find({}).exec()
-	.then(function (users) {
-		res.json(users);
+router.get('/', function(req, res, next) {
+  User.find({}).exec()
+	.then(function(users) {
+  var modUsers = users.map(function(user) {
+    delete user.password;
+    return user;
+  });
+  res.json(modUsers);
 	})
 	.then(null, next);
 });
 
-router.post('/', function (req, res, next) {
-	User.create(req.body)
-	.then(function (user) {
-		res.status(201).json(user);
+router.post('/', function(req, res, next) {
+  User.create(req.body)
+	.then(function(user) {
+  res.status(201).json(user);
 	})
 	.then(null, next);
 });
 
-router.get('/:id', function (req, res, next) {
-	req.requestedUser.getStories()
-	.then(function (stories) {
-		var obj = req.requestedUser.toObject();
-		obj.stories = stories;
-		res.json(obj);
+router.get('/:id', function(req, res, next) {
+  req.requestedUser.getStories()
+	.then(function(stories) {
+  var obj = req.requestedUser.toObject();
+  obj.stories = stories;
+  delete obj.password;
+  res.json(obj);
 	})
 	.then(null, next);
 });
 
-router.put('/:id', function (req, res, next) {
-	_.extend(req.requestedUser, req.body);
-	req.requestedUser.save()
-	.then(function (user) {
-		res.json(user);
+router.put('/:id', function(req, res, next) {
+  _.extend(req.requestedUser, req.body);
+  req.requestedUser.save()
+	.then(function(user) {
+  res.json(user);
 	})
 	.then(null, next);
 });
 
-router.delete('/:id', function (req, res, next) {
-	req.requestedUser.remove()
-	.then(function () {
-		res.status(204).end();
+router.delete('/:id', function(req, res, next) {
+  req.requestedUser.remove()
+	.then(function() {
+  res.status(204).end();
 	})
 	.then(null, next);
 });
